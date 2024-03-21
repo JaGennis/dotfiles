5b976a91686a9ac0a499e0620dbb2df0335b6183
Jannis Geuppert <jannis.geuppert@maibornwolff.de>
2024-03-21 11:19:02 +0100

    Make mason strict, use lazy keys

--- .config/nvim/lua/plugins/mason-lspconfig.lua
+++ .config/nvim/lua/plugins/mason-lspconfig.lua
@@ -4,6 +4,17 @@ local plugin = {
         'neovim/nvim-lspconfig',
         'williamboman/mason.nvim',
     },
+    lazy = false,
+    keys = {
+        {"<C-k>", vim.lsp.buf.signature_help },
+        {"leader>f", function() vim.lsp.buf.format { async = true } end },
+        {"<leader>q", vim.diagnostic.setloclist },
+        {'gD', vim.lsp.buf.declaration },
+        {'gd', vim.lsp.buf.definition },
+        {'gi', vim.lsp.buf.implementation },
+        {'gt', vim.lsp.buf.type_definition },
+        {'gr', vim.lsp.buf.references },
+    },
     config = function()
         local capabilities = require('cmp_nvim_lsp').default_capabilities()
         -- require('neodev').setup()
@@ -24,21 +35,6 @@ local plugin = {
         require('mason-lspconfig').setup({
             automatic_installation = true
         })
-
-        local opts = { noremap = true, silent = true }
-        vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, opts)
-        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-        vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, opts)
-        vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-        -- vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-        vim.keymap.set('n', '<leader>f', function() vim.lsp.buf.format { async = true } end, opts)
-        -- vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-        -- vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-        -- vim.keymap.set('n', '<space>wl', function()
-        --   print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-        -- end, opts)
 
         vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
 
