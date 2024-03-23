3a1bae4211a7ca9b29637c967a219f0faba29ed1
Jannis Geuppert <jannis.geuppert@maibornwolff.de>
2024-03-23 15:04:33 +0100

    Remove redundant line

--- .config/nvim/lua/general.lua
+++ .config/nvim/lua/general.lua
@@ -1,6 +1,5 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.smartcase = true
 
 vim.opt.smarttab = true
 vim.opt.autoindent = true
@@ -8,6 +7,7 @@ vim.opt.smartindent = true
 vim.opt.expandtab = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
+
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
