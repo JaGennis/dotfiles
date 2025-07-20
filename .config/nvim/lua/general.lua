8e56a289ff6f1db7d35c785f66fa369fba648c24
Jannis Geuppert <jannis.geuppert@maibornwolff.de>
2025-07-20 23:29:21 +0200

    Add keymaps to resize windows

--- .config/nvim/lua/general.lua
+++ .config/nvim/lua/general.lua
@@ -24,6 +24,10 @@ vim.keymap.set('n', '<C-l>', '<C-W>l')
 vim.keymap.set('n', '<Leader>rr', ':luafile %<CR>')
 vim.keymap.set('n', 'n', 'nzz')
 vim.keymap.set('n', 'N', 'Nzz')
+vim.keymap.set('n', '=', ':vertical resize +5<CR>')
+vim.keymap.set('n', '-', ':vertical resize -5<CR>')
+vim.keymap.set('n', '+', ':horizontal resize +5<CR>')
+vim.keymap.set('n', '_', ':horizontal resize -5<CR>')
 vim.keymap.set('c', 'w!!', "silent! write ! doas tee % > /dev/null", { silent = true })
 
 vim.g.loaded_netrw = 1
