b6613475321d9115eb746d252da98bf6741c2f67
Jannis Geuppert <jannis.geuppert@maibornwolff.de>
2025-07-25 20:07:43 +0200

    Replace null-ls by none-ls

--- .config/nvim/lua/plugins/null-ls.lua
+++ .config/nvim/lua/plugins/null-ls.lua
@@ -1,10 +1,13 @@
 return {
-    'jose-elias-alvarez/null-ls.nvim',
+    'nvimtools/none-ls.nvim',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+    },
     config = function()
         local null_ls = require("null-ls")
         null_ls.setup({
             sources = {
-                null_ls.builtins.code_actions.gitsigns,
+                -- null_ls.builtins.code_actions.gitsigns,
                 null_ls.builtins.formatting.prettier,
             },
         })
