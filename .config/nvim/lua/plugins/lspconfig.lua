dca160ee043db41d622b9af8c4b40dac934e7f7a
JaGennis <JaGennis@users.noreply.github.com>
2025-07-29 15:09:13 +0200

    Replace telescope by snacks

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -7,11 +7,6 @@ return {
   keys = {
       {"<leader>fo", function() vim.lsp.buf.format { async = true } end, desc = "Format buffer" },
       {"<leader>q", vim.diagnostic.setloclist, desc = "Show diagnostics" },
-      {'gD', vim.lsp.buf.declaration, desc = "Go to declaration"},
-      {'gd', vim.lsp.buf.definition, desc = "Go to definition" },
-      {'gi', vim.lsp.buf.implementation, desc = "Go to implementation" },
-      {'gt', vim.lsp.buf.type_definition, desc = "Go to type definition" },
-      {'gr', vim.lsp.buf.references, desc = "Go to references" },
       { "<leader>ch", "<cmd>ClangdSwitchSourceHeader<cr>", desc = "Switch Source/Header (C/C++)"},
   },
   lazy = false,
---
+++ .config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,45 @@
+return {
+    "folke/snacks.nvim",
+    lazy = false,
+    opts = {
+        explorer = {
+            hidden = true
+        }
+    },
+    keys = {
+        { "<leader>e", function() Snacks.explorer() end, desc = "File Explorer"},
+        { "<leader>:", function() Snacks.picker.command_history() end, desc = "Command History"},
+        { "<leader>/", function() Snacks.picker.grep() end, desc = "Grep"},
+        { "<leader>n", function() Snacks.picker.notifications() end, desc = "Notification History"},
+
+        -- find
+        { "<leader>ff", function() Snacks.picker.files() end, desc = "Find Files"},
+        { "<leader>fc", function() Snacks.picker.files({ cwd = vim.fn.stdpath("config")}) end, desc = "Find Config Files"},
+        { "<leader>fb", function() Snacks.picker.buffers() end, desc = "Buffers"},
+
+        -- search
+        { "<leader>gw", function() Snacks.picker.grep_word() end, desc = "Grep Word", mode = { "n", "x" }},
+        { "<leader>km", function() Snacks.picker.keymaps() end, desc = "Keymaps"},
+        { "<leader>dd", function() Snacks.picker.diagnostics() end, desc = "Diagnostics"},
+        { "<leader>db", function() Snacks.picker.diagnostics_buffer() end, desc = "Diagnostics Buffer"},
+        { "<leader>ic", function() Snacks.picker.icons() end, desc = "Icons"},
+        { "<leader>uh", function() Snacks.picker.undo() end, desc = "Undo History"},
+
+        -- git
+        { "<leader>gd", function() Snacks.picker.git_diff() end, desc = "Git Diff"},
+        { "<leader>gst", function() Snacks.picker.git_status() end, desc = "Git Status"},
+        { "<leader>gb", function() Snacks.picker.git_branches() end, desc = "Git Branches"},
+        { "<leader>glg", function() Snacks.picker.git_log() end, desc = "Git Log"},
+        { "<leader>gll", function() Snacks.picker.git_log_line() end, desc = "Git Log Line"},
+        { "<leader>glf", function() Snacks.picker.git_log_file() end, desc = "Git Log File"},
+
+        -- LSP
+        { "gd", function() Snacks.picker.lsp_definitions() end, desc = "Goto Definition" },
+        { "gD", function() Snacks.picker.lsp_declarations() end, desc = "Goto Declaration" },
+        { "gr", function() Snacks.picker.lsp_references() end, nowait = true, desc = "References" },
+        { "gi", function() Snacks.picker.lsp_implementations() end, desc = "Goto Implementation" },
+        { "gt", function() Snacks.picker.lsp_type_definitions() end, desc = "Goto Type Definition" },
+        { "<leader>ls", function() Snacks.picker.lsp_symbols() end, desc = "LSP Symbols" },
+        { "<leader>ws", function() Snacks.picker.lsp_workspace_symbols() end, desc = "LSP Workspace Symbols" },
+    }
+}
