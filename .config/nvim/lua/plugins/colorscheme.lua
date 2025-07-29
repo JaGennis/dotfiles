43d732a826f104d1e2ea6b9bf4f0bbcbb1c28cab
JaGennis <JaGennis@users.noreply.github.com>
2025-07-29 12:16:44 +0200

    Change colorscheme to onedarkpro, add tokyonight

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,32 @@
 return {
-    'navarasu/onedark.nvim',
-    config = function()
-        require('onedark').setup({
+    -- {
+    --     'navarasu/onedark.nvim',
+    --     config = function()
+    --         require('onedark').setup({
+    --             transparent = true,
+    --         })
+    --         require('onedark').load()
+    --     end
+    -- },
+    {
+        "olimorris/onedarkpro.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require("onedarkpro").setup({
+                options = {
+                    transparency = true,
+                }
+            })
+            vim.cmd("colorscheme onedark")
+        end
+    },
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {
             transparent = true,
-        })
-        require('onedark').load()
-    end
+        },
+    }
 }
