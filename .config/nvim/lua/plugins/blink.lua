f35dd1b9e580b3a38887a8e22b093ad1c2c9b7af
JaGennis <JaGennis@users.noreply.github.com>
2025-08-04 00:29:03 +0200

    Make tab and s-tab scroll through snippets

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -1,22 +1,38 @@
 return {
-  'saghen/blink.cmp',
-  version = '1.*',
-  -- build = 'cargo build --release',
-  opts = {
-    keymap = {
-        preset = 'enter'
-    },
-      sources = {
-        default = { "lazydev", "lsp", "path", "snippets", "buffer" },
-        providers = {
-          lazydev = {
-            name = "LazyDev",
-            module = "lazydev.integrations.blink",
-            score_offset = 100,
-          },
+    'saghen/blink.cmp',
+    version = '1.*',
+    -- build = 'cargo build --release',
+    opts = {
+        cmdline = {
+            keymap = {
+                ["<CR>"] = {'accept', 'fallback'}
+            }
         },
-      },
-  },
+        keymap = {
+            preset = 'enter',
+            ["<Tab>"] = {
+                "select_next",
+                "snippet_forward",
+                "fallback",
+            },
+
+            ["<S-Tab>"] = {
+                "select_prev",
+                "snippet_backward",
+                "fallback",
+            },
+        },
+        sources = {
+            default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+            providers = {
+                lazydev = {
+                    name = "LazyDev",
+                    module = "lazydev.integrations.blink",
+                    score_offset = 100,
+                },
+            },
+        },
+    },
     -- fuzzy = { implementation = "prefer_rust_with_warning" }
-  opts_extend = { "sources.default" }
+    opts_extend = { "sources.default" }
 }
