a5ef18d8fb477fc98c9ca210c92212c24b602569
Jannis Geuppert <jannis.geuppert@maibornwolff.de>
2025-08-04 13:59:26 +0200

    Integrate CopilotChat.nvim

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -1,21 +1,41 @@
 return {
-    "github/copilot.vim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    cmd = "Copilot",
-    event = "BufWinEnter",
-    init = function()
-        vim.g.copilot_no_maps = true
-    end,
-    config = function()
-        -- Block the normal Copilot suggestions
-        vim.api.nvim_create_augroup("github_copilot", { clear = true })
-        vim.api.nvim_create_autocmd({ "FileType", "BufUnload" }, {
-            group = "github_copilot",
-            callback = function(args)
-                vim.fn["copilot#On" .. args.event]()
-            end,
-        })
-        vim.fn["copilot#OnFileType"]()
-    end,
-
+    {
+        "github/copilot.vim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        cmd = "Copilot",
+        event = "BufWinEnter",
+        init = function()
+            vim.g.copilot_no_maps = true
+        end,
+        config = function()
+            -- Block the normal Copilot suggestions
+            vim.api.nvim_create_augroup("github_copilot", { clear = true })
+            vim.api.nvim_create_autocmd({ "FileType", "BufUnload" }, {
+                group = "github_copilot",
+                callback = function(args)
+                    vim.fn["copilot#On" .. args.event]()
+                end,
+            })
+            vim.fn["copilot#OnFileType"]()
+        end,
+    },
+    {
+        "CopilotC-Nvim/CopilotChat.nvim",
+        dependencies = "nvim-lua/plenary.nvim",
+        build = "make tiktoken",
+        opts = {
+            -- See Configuration section for options
+        },
+        keys = {
+            { "<leader>cc", "<cmd>CopilotChatToggle<CR>",   desc = "Toggle copilot chat window", mode = { "n", "x" } },
+            { "<leader>cr", "<cmd>CopilotChatReview<CR>",   desc = "Review selected code",       mode = { "n", "x" } },
+            { "<leader>cg", "<cmd>CopilotChatCommit<CR>",   desc = "Generate commit message",    mode = { "n", "x" } },
+            { "<leader>ce", "<cmd>CopilotChatExplain<CR>",  desc = "Explain selected code",      mode = { "n", "x" } },
+            { "<leader>cf", "<cmd>CopilotChatFix<CR>",      desc = "Fix selected code",          mode = { "n", "x" } },
+            { "<leader>co", "<cmd>CopilotChatOptimize<CR>", desc = "Optimize selected code",     mode = { "n", "x" } },
+            { "<leader>cd", "<cmd>CopilotChatDocs<CR>",     desc = "Document selected code",     mode = { "n", "x" } },
+        }
+        -- {"<leader>k", "<cmd>Lspsaga hover_doc<CR>", desc = "Show documentation"},
+        -- {"as", mode = {'o', 'x'}, function() require("various-textobjs").subword("outer") end, desc = "Outer subword"},
+    },
 }
