b723dc87eaec72f27da33ed57e36dcec99455358
Jannis Geuppert <jannis.geuppert@maibornwolff.de>
2025-08-11 16:04:25 +0200

    Add cmake-tools, fix dependencies of markdown.lua

---
+++ .config/nvim/lua/plugins/cmake.lua
@@ -0,0 +1,8 @@
+return {
+    'Civitasv/cmake-tools.nvim',
+    opts = {},
+    keys = {
+        { "<leader>cmb", "<cmd>CMakeBuild<cr>", desc = "Build with CMake" },
+        { "<leader>cmr", "<cmd>CMakeRun<cr>",   desc = "Run CMake Target" },
+    },
+}
